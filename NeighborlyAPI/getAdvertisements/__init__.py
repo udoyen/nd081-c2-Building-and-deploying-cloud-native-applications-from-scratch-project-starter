b7e7cdc94b096566aa00015657f163854b7811bc
udoyen <datameshprojects@gmail.com>
2021-04-28 22:45:57 +0100

    add image and finished email trigger

--- NeighborlyAPI/getAdvertisements/__init__.py
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -1,9 +1,25 @@
+# Requires pip install requests && pip freeze > requirements.txt
+import requests
+import os
 import azure.functions as func
 import pymongo
 import json
 from bson.json_util import dumps
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        payload = {
+            "email": "ekojojoshua@hotmail.com",
+            "due": "28/4/2021",
+            "task": "My new task!"
+        }
+        response = requests.post(
+            os.environ['LOGIC_APP_URL'], data = payload)
+        print(f"Success: {response.status_code}")
+    except Exception as e:
+        print(f"Mail was not sent! {e}")
+
 
     try:
         url = "mongodb://myneighborlycosmoacc:FfPutq1TardvkmWnodcUSCYvRssgDiZuToNjR4YX4fUI4H2TZ1vWFFpadEy3Q1wi7Hc5jL8U6B7yIivQsLRpAw==@myneighborlycosmoacc.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@myneighborlycosmoacc@"
